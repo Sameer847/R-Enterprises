2b541e9af66a52dfded3cb1118db3d1ce60dc92f
Zahid Ibrahim Shaikh <53161242+The-Lone-Druid@users.noreply.github.com>
2020-02-17 13:43:05 +0530

    Added some new features

--- prototype.js
+++ prototype.js
@@ -1,16 +1,4 @@
 //Functions for opening and closing side menu bar, only at device width == 600px;
-function openMenu(){
-    document.getElementById("openbtn").style.display='none';
-    document.getElementById("closebtn").style.display='inline-block';
-    document.getElementById("side-menu").style.display='block';
-}
-
-function closeMenu(){
-    document.getElementById("closebtn").style.display='none';
-    document.getElementById("openbtn").style.display='inline-block';
-    document.getElementById("side-menu").style.display='none';
-}
-
-function changeTheme(){
-    document.getElementById("theme-select").style.backgroundColor = 'grey';
+function activateSearch(){
+    
 }
